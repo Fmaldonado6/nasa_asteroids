df5acd5c3988e7c00e309701f78f3c1b9fe34054
Fernando Maldonado <f_maldonado6@hotmail.com>
2021-10-01 17:37:09 -0500

    Finished project

--- code/lib/main.dart
+++ code/lib/main.dart
@@ -11,12 +11,36 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          textTheme: TextTheme(
+            headline6: Theme.of(context)
+                .textTheme
+                .headline6
+                ?.apply(color: Colors.black),
+          ),
+        ),
       ),
+      darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.blue,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            textTheme: TextTheme(
+              headline6: Theme.of(context)
+                  .textTheme
+                  .headline6
+                  ?.apply(color: Colors.white),
+            ),
+          ),
+          scaffoldBackgroundColor: Color(0xFF202125),
+          cardColor: Color(0xFF242529)),
       home: MainScreen(),
     );
   }
 }
-
-
--- code/lib/screens/main_screen.dart
+++ code/lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:taller_flutter/models/models.dart';
 import 'package:taller_flutter/services/nasa_service.dart';
 import 'package:taller_flutter/widgets/asteroid_card.dart';
@@ -16,14 +17,71 @@ class _MainScreenState extends State<MainScreen> {
   DateTime startDate = DateTime.now();
   DateTime endDate = DateTime.now();
 
+  Future openDateRangeSelector() async {
+    var dateRange = await showDateRangePicker(
+      context: context,
+      initialDateRange: DateTimeRange(start: startDate, end: endDate),
+      firstDate: DateTime(2016),
+      lastDate: DateTime.now(),
+    );
+
+    if (dateRange == null) return;
+
+    var difference = dateRange.start.difference(dateRange.end);
+
+    if (difference.inDays.abs() > 7) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Ingrese un rango menor a 7 dias"),
+      ));
+      return;
+    }
+
+    setState(() {
+      startDate = dateRange.start;
+      endDate = dateRange.end;
+    });
+  }
+
+  bool isDarkModeEnabled() {
+    return MediaQuery.of(context).platformBrightness == Brightness.dark;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backwardsCompatibility: false,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarIconBrightness:
+              isDarkModeEnabled() ? Brightness.light : Brightness.dark,
+        ),
+        title: Text(
+          "Asteroids",
+          style: TextStyle(
+            color: isDarkModeEnabled() ? Colors.white : Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.date_range),
+            onPressed: openDateRangeSelector,
+            color: isDarkModeEnabled() ? Colors.white : Colors.black,
+          )
+        ],
+      ),
       body: Column(
         children: [
-          Text("TExto 1"),
           Expanded(
+            flex: 3,
+            child: Container(
+              width: MediaQuery.of(context).size.width * .5,
+              child: Image.asset("assets/earth.gif"),
+            ),
+          ),
+          Expanded(
+            flex: 2,
             child: FutureBuilder(
               future: nasaService.getAsteroids(startDate, endDate),
               builder: (context, snapshot) {
